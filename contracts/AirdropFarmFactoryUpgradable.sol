dbd688765e8ce285877c0f4665cf8395c0769c66
Neo <entropizm@gmail.com>
2024-11-11 19:55:46 +0700

    Logic fixes
    Broken update

--- contracts/AirdropFarmFactoryUpgradable.sol
+++ contracts/AirdropFarmFactoryUpgradable.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.27;
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "./AirdropFarmingPoolUpgradeable.sol";
+import "./AirdropFarmingPoolUpgradable.sol";
 
 contract AirdropFarmFactoryUpgradeable is Initializable, UUPSUpgradeable, AccessControlUpgradeable {
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
@@ -20,15 +20,15 @@ contract AirdropFarmFactoryUpgradeable is Initializable, UUPSUpgradeable, Access
         __AccessControl_init();
         __UUPSUpgradeable_init();
 
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _setupRole(ADMIN_ROLE, msg.sender);
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(ADMIN_ROLE, msg.sender);
     }
 
     function _authorizeUpgrade(address newImplementation) internal override onlyRole(ADMIN_ROLE) {}
 
     function createFarm(
         string calldata _farmName,
-        IERC20Upgradeable _acceptedToken,
+        ERC20Upgradeable _acceptedToken,
         uint256 _baseRate,
         uint256 _timeLockedMultiplier,
         uint256 _donationMultiplier
--- ignition/modules/Lock.ts
+++ ignition/modules/Lock.ts
@@ -1,20 +1,29 @@
-// This setup uses Hardhat Ignition to manage smart contract deployments.
-// Learn more about it at https://hardhat.org/ignition
+import { ethers, upgrades } from "hardhat";
 
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log(`Deploying contracts with deployer: ${deployer.address}`);
 
-const JAN_1ST_2030 = 1893456000;
-const ONE_GWEI: bigint = 1_000_000_000n;
-
-const LockModule = buildModule("LockModule", (m) => {
-  const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
-  const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
-
-  const lock = m.contract("Lock", [unlockTime], {
-    value: lockedAmount,
+  // Deploy the AirdropFarmFactoryUpgradeable as a UUPS proxy
+  const Factory = await ethers.getContractFactory("AirdropFarmFactoryUpgradeable");
+  const factory = await upgrades.deployProxy(Factory, [], {
+    kind: "uups",
+    initializer: "initialize",
   });
 
-  return { lock };
-});
+  await factory.deployed();
 
-export default LockModule;
+  console.log(`AirdropFarmFactoryUpgradeable deployed at: ${factory.address}`);
+
+  // Fetch the implementation address
+  const implementationAddress = await upgrades.erc1967.getImplementationAddress(factory.address);
+  console.log(`AirdropFarmFactoryUpgradeable implementation deployed at: ${implementationAddress}`);
+}
+
+// Execute the script
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
